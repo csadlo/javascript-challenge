b13fa5ceb39f53d0a169a1b9f7e86276633fc9ee
Christopher Sadlo <chris.sadlo@gmail.com>
2020-10-15 20:19:36 -0500

    Completed Level One - I Think

--- static/js/app.js
+++ static/js/app.js
@@ -1,24 +1,58 @@
 // from data.js
 var tableData = data;
+var datetime = d3.select("#datetime");
 
-// Get a reference to the table body
-var tbody = d3.select("tbody");
+function updateTable(thisTableData) {
 
-// Console.log the weather data from data.js
-console.log(tableData);
+  console.log("Hello There!");
 
-// Use d3 to append one table row `tr` for each UFO-sighting report object
-data.forEach(function(sightingReport) {
-  console.log(sightingReport);
-  var row = tbody.append("tr");
+  // Start from scratch
+  d3.selectAll("tr").remove();
 
-  Object.entries(sightingReport).forEach(function([key, value]) {
-    console.log(key, value);
+  // Get a reference to the table body
+  var tbody = d3.select("tbody");
 
-    // Append a cell to the row for each value in the UFO-sighting report object
-    var cell = row.append("td");
-    cell.text(value);
+  // Console.log the weather data from data.js
+  console.log(thisTableData);
+
+  // Use d3 to append one table row `tr` for each UFO-sighting report object
+  thisTableData.forEach(function(sightingReport) {
+    //console.log(sightingReport);
+    var row = tbody.append("tr");
+
+    Object.entries(sightingReport).forEach(function([key, value]) {
+      //console.log(key, value);
+
+      // Append a cell to the row for each value in the UFO-sighting report object
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
+}
 
 
-});
+//function ByExactDate(sightingReport) {
+//  return sightingReport.datetime === 
+//}
+
+// Function to handle datetime filter input change
+function handleChange(event) {
+
+  // grab the value of the input field
+  var inputText = d3.event.target.value;
+
+  console.log(inputText);
+
+  var filteredData = tableData.filter(sightingReport => sightingReport.datetime === inputText);
+
+  console.log(filteredData);
+
+  updateTable(filteredData);
+
+  return false;
+}
+
+datetime.on("change", handleChange);
+
+
+updateTable(tableData);
